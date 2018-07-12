e61b497a689816f49a63999ed9b5fdac1be0d41d
gggia <gggia36@gmail.com>
2018-07-12 17:11:32 +0700

    y

---
+++ about-us.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+  <!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+    <!-- END NAVBAR -->
+      <div class="wrapper">
+
+        <!-- CONTENT -->
+
+      
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 mt-4">
+              <div>
+                <h1 class="">เกี่ยวกับเรา(About us)</h1>
+              </div>
+            </div>
+            <hr class="about-hr-y">
+          </div>
+          <div class="row">
+            <div class="col-lg-12 mt-4">
+              <h3 class="text-center">บริษัท ไทย เอ็น. เค. พลาสติก จำกัด</h3>
+            </div>
+            <div class="col-lg-12 text-center">
+              <p>8/8 หมู่ 4 ซอย เทศบาล 19 ถนนบางกรวย-ไทยน้อย ตำบลโสนลอย</p>
+              <p>อำเภอ บางบัวทอง จังหวัดนนทบุรี 11110</p>
+              <p>Tel : +66 02-9202500-5 Fax : +66 02-9202506</p>
+              <p>www.thainkplastic.co.th</p>
+            </div>
+            
+          </div>
+          
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+        
+
+      
+
+
+
+
+
+
+
+        <!-- END CONTENT -->
+      </div>
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
