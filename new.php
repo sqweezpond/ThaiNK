6487f24869ceab1478c346d72e1c1abc53c4efbb
gggia <gggia36@gmail.com>
2018-07-13 16:15:19 +0700

    up

--- new.php
+++ new.php
@@ -10,7 +10,7 @@
   <!-- START NAVBAR -->
     <?php include_once('inc/navbar.php'); ?>
     <!-- END NAVBAR -->
-      <div class="wrapper">
+      <div class="wrapper fadeIn animated">
 
         <!-- CONTENT -->
 
@@ -28,6 +28,108 @@
         </div>
       </div>
  <!-- end bg-top -->
+
+ <div class="container">
+   <div class="row my-5">
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="new_card ">
+        <div class="new_frame-crad">
+          <a href="#"><img class="new_card-img-top" src="http://www.thailandindustry.com/ex_uploads/9ce762f59e4b08c8091810d12e29a0c5.JPG" alt="Card image cap"></a>
+        </div>
+        <div class="card-body">
+          <a href="#"><h5 class="new_fix_text">Lorem Ipsum คืออะไร?</h5></a>
+          <div class="news_fix_text_3">
+            <p class="card-text ">มีหลักฐานที่เป็นข้อเท็จจริงยืนยันมานานแล้ว ว่าเนื้อหาที่อ่านรู้เรื่องนั้นจะไปกวนสมาธิของคนอ่านให้เขวไปจากส่วนที้เป็น Layout เรานำ Lorem Ipsum มาใช้เพราะความที่มันมีการกระจายของตัวอักษรธรรมดาๆ</p>
+          </div>
+          <div>
+            <a href="#" class="color-rad">ดูรายละเอียด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="new_card ">
+        <div class="new_frame-crad">
+          <a href="#"><img class="new_card-img-top" src="https://site-assets.mediaoxide.com/workpointnews/2018/02/16085341/%E0%B9%80%E0%B8%A5%E0%B8%B4%E0%B8%81%E0%B9%83%E0%B8%8A%E0%B9%89-%E0%B8%9E%E0%B8%A5%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%B4%E0%B8%81-1200x630.jpg" alt="Card image cap"></a>
+        </div>
+        <div class="card-body">
+          <a href="#"><h5 class="new_fix_text">Lorem Ipsum คืออะไร?</h5></a>
+          <div class="news_fix_text_3">
+            <p class="card-text ">มีหลักฐานที่เป็นข้อเท็จจริงยืนยันมานานแล้ว ว่าเนื้อหาที่อ่านรู้เรื่องนั้นจะไปกวนสมาธิของคนอ่านให้เขวไปจากส่วนที้เป็น Layout เรานำ Lorem Ipsum มาใช้เพราะความที่มันมีการกระจายของตัวอักษรธรรมดาๆ</p>
+          </div>
+          <div>
+            <a href="#" class="color-rad">ดูรายละเอียด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="new_card ">
+        <div class="new_frame-crad">
+          <a href="#"><img class="new_card-img-top" src="https://site-assets.mediaoxide.com/workpointnews/2018/02/16085341/%E0%B9%80%E0%B8%A5%E0%B8%B4%E0%B8%81%E0%B9%83%E0%B8%8A%E0%B9%89-%E0%B8%9E%E0%B8%A5%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%B4%E0%B8%81-1200x630.jpg" alt="Card image cap"></a>
+        </div>
+        <div class="card-body">
+          <a href="#"><h5 class="new_fix_text">Lorem Ipsum คืออะไร?</h5></a>
+          <div class="news_fix_text_3">
+            <p class="card-text ">มีหลักฐานที่เป็นข้อเท็จจริงยืนยันมานานแล้ว ว่าเนื้อหาที่อ่านรู้เรื่องนั้นจะไปกวนสมาธิของคนอ่านให้เขวไปจากส่วนที้เป็น Layout เรานำ Lorem Ipsum มาใช้เพราะความที่มันมีการกระจายของตัวอักษรธรรมดาๆ</p>
+          </div>
+          <div>
+            <a href="#" class="color-rad">ดูรายละเอียด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="new_card ">
+        <div class="new_frame-crad">
+          <a href="#"><img class="new_card-img-top" src="https://site-assets.mediaoxide.com/workpointnews/2018/02/16085341/%E0%B9%80%E0%B8%A5%E0%B8%B4%E0%B8%81%E0%B9%83%E0%B8%8A%E0%B9%89-%E0%B8%9E%E0%B8%A5%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%B4%E0%B8%81-1200x630.jpg" alt="Card image cap"></a>
+        </div>
+        <div class="card-body">
+          <a href="#"><h5 class="new_fix_text">Lorem Ipsum คืออะไร?</h5></a>
+          <div class="news_fix_text_3">
+            <p class="card-text ">มีหลักฐานที่เป็นข้อเท็จจริงยืนยันมานานแล้ว ว่าเนื้อหาที่อ่านรู้เรื่องนั้นจะไปกวนสมาธิของคนอ่านให้เขวไปจากส่วนที้เป็น Layout เรานำ Lorem Ipsum มาใช้เพราะความที่มันมีการกระจายของตัวอักษรธรรมดาๆ</p>
+          </div>
+          <div>
+            <a href="#" class="color-rad">ดูรายละเอียด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="new_card ">
+        <div class="new_frame-crad">
+          <a href="#"><img class="new_card-img-top" src="https://site-assets.mediaoxide.com/workpointnews/2018/02/16085341/%E0%B9%80%E0%B8%A5%E0%B8%B4%E0%B8%81%E0%B9%83%E0%B8%8A%E0%B9%89-%E0%B8%9E%E0%B8%A5%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%B4%E0%B8%81-1200x630.jpg" alt="Card image cap"></a>
+        </div>
+        <div class="card-body">
+          <a href="#"><h5 class="new_fix_text">Lorem Ipsum คืออะไร?</h5></a>
+          <div class="news_fix_text_3">
+            <p class="card-text ">มีหลักฐานที่เป็นข้อเท็จจริงยืนยันมานานแล้ว ว่าเนื้อหาที่อ่านรู้เรื่องนั้นจะไปกวนสมาธิของคนอ่านให้เขวไปจากส่วนที้เป็น Layout เรานำ Lorem Ipsum มาใช้เพราะความที่มันมีการกระจายของตัวอักษรธรรมดาๆ</p>
+          </div>
+          <div>
+            <a href="#" class="color-rad">ดูรายละเอียด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="new_card ">
+        <div class="new_frame-crad">
+          <a href="#"><img class="new_card-img-top" src="https://site-assets.mediaoxide.com/workpointnews/2018/02/16085341/%E0%B9%80%E0%B8%A5%E0%B8%B4%E0%B8%81%E0%B9%83%E0%B8%8A%E0%B9%89-%E0%B8%9E%E0%B8%A5%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%B4%E0%B8%81-1200x630.jpg" alt="Card image cap"></a>
+        </div>
+        <div class="card-body">
+          <a href="#"><h5 class="new_fix_text">Lorem Ipsum คืออะไร?</h5></a>
+          <div class="news_fix_text_3">
+            <p class="card-text ">มีหลักฐานที่เป็นข้อเท็จจริงยืนยันมานานแล้ว ว่าเนื้อหาที่อ่านรู้เรื่องนั้นจะไปกวนสมาธิของคนอ่านให้เขวไปจากส่วนที้เป็น Layout เรานำ Lorem Ipsum มาใช้เพราะความที่มันมีการกระจายของตัวอักษรธรรมดาๆ</p>
+          </div>
+          <div>
+            <a href="#" class="color-rad">ดูรายละเอียด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+   </div>
+ </div>
 
 
 
