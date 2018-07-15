9f65a5fd99ae1308505b93a2b6d0d855cd9af2a1
Parinya Yodming <sqweezpond>
2018-07-15 16:15:25 +0700

    update
    update (pond)

---
+++ product-detail.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+  <!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+  <!-- END NAVBAR -->
+
+    <div class="wrapper">
+       <!-- CONTENT -->
+        <div class="container pb-5">
+            <div class="row mt-5">
+                <div class="col-xl-7">
+                  <!-- BLOCK SLIDE --------------------------------------------------->
+                      <div class="product-overlay-slide">
+                            <img class="mySlides product-image-slide" src="assets/images/slide/s1.jpg">
+                            <img class="mySlides product-image-slide" src="assets/images/slide/s2.jpg">
+                            <img class="mySlides product-image-slide" src="assets/images/slide/s3.jpg">
+                            <img class="mySlides product-image-slide" src="assets/images/slide/s4.jpg">
+                            <img class="mySlides product-image-slide" src="assets/images/slide/s5.jpg">
+                            <img class="mySlides product-image-slide" src="assets/images/slide/s6.jpg">
+                      </div>
+                  <!-- SLIDE ------------------------------------------------------->
+                      <div class="owl-carousel owl-theme">
+                              <div class="item p-1">
+                                  <div class="product-overlay-coulasel">
+                                      <img src = "assets/images/slide/s1.jpg" class="product-image-coulasel demo" onclick="currentDiv(1)">
+                                  </div>
+                              </div>
+                              <div class="item p-1">
+                                  <div class="product-overlay-coulasel">
+                                      <img src = "assets/images/slide/s2.jpg" class="product-image-coulasel demo" onclick="currentDiv(2)">
+                                  </div>
+                              </div>
+                              <div class="item p-1">
+                                  <div class="product-overlay-coulasel">
+                                      <img src = "assets/images/slide/s3.jpg" class="product-image-coulasel demo" onclick="currentDiv(3)">
+                                  </div>
+                              </div>
+                              <div class="item p-1">
+                                  <div class="product-overlay-coulasel">
+                                      <img src = "assets/images/slide/s4.jpg" class="product-image-coulasel demo" onclick="currentDiv(4)">
+                                  </div>
+                              </div>
+                              <div class="item p-1">
+                                  <div class="product-overlay-coulasel">
+                                      <img src = "assets/images/slide/s5.jpg" class="product-image-coulasel demo" onclick="currentDiv(5)">
+                                  </div>
+                              </div>
+                              <div class="item p-1">
+                                  <div class="product-overlay-coulasel">
+                                      <img src = "assets/images/slide/s6.jpg" class="product-image-coulasel demo" onclick="currentDiv(6)">
+                                  </div>
+                              </div>
+                      </div>
+                </div>
+                  <!-- END SLIDE ------------------------------------------->
+                <div class="col-xl-5">
+                  dbbsbs
+                </div>
+            </div>
+
+
+            <!-- Coulasel ------------------------------------------------------->
+            <div class="row mt-5">
+                  <div class="row owl-carousel owl-theme mt-5 mx-0">
+                          <div class="item p-1">
+                              <div class="home-coulasel">
+                                  <div class="card p-3 b-n">
+                                      <div class="home-overlay-coulasel">
+                                        <img src = "assets/images/banner/banner-1.png" class="home-image-coulasel">
+                                      </div>
+                                      <div class="card-body px-0 pb-0">
+                                        <span class="home-font9">200 บาท</span>
+                                        <span class="home-font10 pl-2">399 บาท</span>
+                                        <div class="home-font11">พลาสติกใสขนาด 6 ลิตร</div>
+                                      </div>
+                                  </div>
+                                  <div class="">
+                                     <button class="btn home-btn3">สั่งซื้อ</button>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="item p-1">
+                              <div class="home-coulasel">
+                                  <div class="card p-3 b-n">
+                                      <div class="home-overlay-coulasel">
+                                        <img src = "assets/images/banner/banner-1.png" class="home-image-coulasel">
+                                      </div>
+                                      <div class="card-body px-0 pb-0">
+                                        <span class="home-font9">200 บาท</span>
+                                        <span class="home-font10 pl-2">399 บาท</span>
+                                        <div class="home-font11">พลาสติกใสขนาด 6 ลิตร</div>
+                                      </div>
+                                  </div>
+                                  <div class="">
+                                     <button class="btn home-btn3">สั่งซื้อ</button>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="item p-1">
+                              <div class="home-coulasel">
+                                  <div class="card p-3 b-n">
+                                      <div class="home-overlay-coulasel">
+                                        <img src = "assets/images/banner/banner-1.png" class="home-image-coulasel">
+                                      </div>
+                                      <div class="card-body px-0 pb-0">
+                                        <span class="home-font9">200 บาท</span>
+                                        <span class="home-font10 pl-2">399 บาท</span>
+                                        <div class="home-font11">พลาสติกใสขนาด 6 ลิตร</div>
+                                      </div>
+                                  </div>
+                                  <div class="">
+                                     <button class="btn home-btn3">สั่งซื้อ</button>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="item p-1">
+                              <div class="home-coulasel">
+                                  <div class="card p-3 b-n">
+                                      <div class="home-overlay-coulasel">
+                                        <img src = "assets/images/banner/banner-1.png" class="home-image-coulasel">
+                                      </div>
+                                      <div class="card-body px-0 pb-0">
+                                        <span class="home-font9">200 บาท</span>
+                                        <span class="home-font10 pl-2">399 บาท</span>
+                                        <div class="home-font11">พลาสติกใสขนาด 6 ลิตร</div>
+                                      </div>
+                                  </div>
+                                  <div class="">
+                                     <button class="btn home-btn3">สั่งซื้อ</button>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="item p-1">
+                              <div class="home-coulasel">
+                                  <div class="card p-3 b-n">
+                                      <div class="home-overlay-coulasel">
+                                        <img src = "assets/images/banner/banner-1.png" class="home-image-coulasel">
+                                      </div>
+                                      <div class="card-body px-0 pb-0">
+                                        <span class="home-font9">200 บาท</span>
+                                        <span class="home-font10 pl-2">399 บาท</span>
+                                        <div class="home-font11">พลาสติกใสขนาด 6 ลิตร</div>
+                                      </div>
+                                  </div>
+                                  <div class="">
+                                     <button class="btn home-btn3">สั่งซื้อ</button>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="item p-1">
+                              <div class="home-coulasel">
+                                  <div class="card p-3 b-n">
+                                      <div class="home-overlay-coulasel">
+                                        <img src = "assets/images/banner/banner-1.png" class="home-image-coulasel">
+                                      </div>
+                                      <div class="card-body px-0 pb-0">
+                                        <span class="home-font9">200 บาท</span>
+                                        <span class="home-font10 pl-2">399 บาท</span>
+                                        <div class="home-font11">พลาสติกใสขนาด 6 ลิตร</div>
+                                      </div>
+                                  </div>
+                                  <div class="">
+                                     <button class="btn home-btn3">สั่งซื้อ</button>
+                                  </div>
+                              </div>
+                          </div>
+                    </div>
+            </div>
+             <!-- END Coulasel ------------------------------------------->
+
+
+
+
+
+
+            <!-- END CONTANER ------>
+        </div>
+        <!-- END CONTENT ---------->
+    </div>
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+    <script type="text/javascript">
+        var slideIndex = 1;
+        showDivs(slideIndex);
+
+        function plusDivs(n) {
+          showDivs(slideIndex += n);
+        }
+
+        function currentDiv(n) {
+          showDivs(slideIndex = n);
+        }
+
+        function showDivs(n) {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          var dots = document.getElementsByClassName("demo");
+          if (n > x.length) {slideIndex = 1}
+          if (n < 1) {slideIndex = x.length}
+          for (i = 0; i < x.length; i++) {
+             x[i].style.display = "none";
+          }
+          for (i = 0; i < dots.length; i++) {
+             dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+          }
+          x[slideIndex-1].style.display = "block";
+          dots[slideIndex-1].className += " w3-opacity-off";
+        }
+    </script>
+  </body>
+</html>
