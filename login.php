cb04b2920c3d48164190a2553a1459d4a6b4aa5b
gggia <gggia36@gmail.com>
2018-07-15 14:38:16 +0700

    up

--- login.php
+++ login.php
@@ -15,6 +15,77 @@
         <!-- CONTENT -->
 
 
+      
+ <div class="container">
+    <div class="row my-5 mx-0 login-text-shado">
+      <div class="col-lg-6">
+        <div class="login_frame-crad login-mar-15">
+            <img class="login_card-img-top w-100" src="assets/images/banner/banner-login4.jpg">
+                <div class="login-position-a ">
+                    <p class="login-font-32 my-3">สำหรับผู้ที่เป็นสมาชิกแล้ว</p>
+                      <p><i class="fa fa-check-circle mr-4 login-color-green" aria-hidden="true"></i>สั่งสินค้าผ่านระบบออนไลน์ได้ตลอด 24 ชม.</p>
+                      <p><i class="fa fa-check-circle mr-4 login-color-green" aria-hidden="true"></i>สิ่งสินค้าผ่านระบบออนไลน์ตั้งแต่ 999 บาท ขึ้นไปรับ<span class="login-color-org">ส่วนลดทันที 10%</span></p>
+                      <p><i class="fa fa-check-circle mr-4 login-color-green" aria-hidden="true"></i>สั่นสินค้าผ่านระบบออนไลน์ได้ตลอด 24 ชม.</p>
+                </div>           
+        </div>
+      </div>
+
+
+      <div class="col-lg-6">
+          <div class="mt-5 text-center">
+            <h2>เข้าสู่ระบบ</h2>
+          </div>
+        <form class="my-4">
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">อีเมล์</label>
+              <div class="col-sm-10">
+                <input type="text" class="login-from" id="inputPassword" placeholder="example@mail.com">
+              </div>
+          </div>
+           <div class="form-group row mt-4">
+                <label for="inputPassword" class="col-sm-2 col-form-label">รหัสผ่าน</label>
+              <div class="col-sm-10">
+                <input type="password" class="login-from" id="inputPassword" placeholder="********">
+              </div>
+            </div>
+        </form>
+           <div class="mt-5 text-center">
+              <button type="button" class="btn login-btn ">เข้าสู่ระบบ</button>
+           </div>
+           <div class="my-3  text-center">
+              <button type="button" class="btn login-btn2"><i class="fa fa-facebook-official mr-4" aria-hidden="true"></i>Login with Facebook</button>
+           </div>
+   
+         <div class="login-display my-4">
+          <div class="col-6">
+            <p class="text-left">ลืมรหัสผ่าน? <a href="#"><span class="login-color-org">คลิกที่นี้</span></a> </p>
+          </div>
+          <div class="col-6">
+            <p class="text-right">หากยังไม่ได้เป็นสมาชิคคลิก <a href="#"><span class="login-color-org " >ลงทะเบียน</span></a></p>
+          </div> 
+        </div>
+
+
+
+      </div>
+    </div>
+ </div>
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
