d7f897069497703db576adc82bf4f22fa7072745
Parinya Yodming <sqweezpond>
2018-07-16 12:24:21 +0700

    update
    update (pond)

---
+++ faq.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+  <!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+  <!-- END NAVBAR -->
+
+    <div class="wrapper">
+       <!-- CONTENT -->
+
+       <!-- bg-top -->
+             <div style=" width: 100%;padding: 8px;background: url('assets/images/banner/title.png'); background-position: right; background-size: cover; " >
+               <div class="container">
+                 <div class="row about_row_bg">
+                   <div class="col-lg-12 mt-4">
+                     <div>
+                       <p class="about-size">คำถามที่พบบ่อย (FAQ)</p>
+                     </div>
+                   </div>
+                   <hr class="about-hr-y">
+                 </div>
+               </div>
+             </div>
+        <!-- end bg-top -->
+
+        <div class="container pb-5">
+          <div class="row mt-5">
+            <div class="col-12">
+                <button class="btn faq-btn" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    <div class="faq-font1">
+                      <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                      เป็นลูกค้า Thai N.K. Plastic จะได้รับสิทธิพิเศษอะไรบ้าง
+                    </div>
+                </button>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body faq-bg">
+                      <div class="faq-font2">ช่วงเวลาการจัดส่งสินค้า คือ จันทร์ - ศุกร์ เวลา 8.00 - 16.00 น. โดยมี 2 ประเภทตามเขตการส่ง ดังนี้ค่ะ</div>
+                      <div class="faq-font2 pt-3">เขตกรุงเทพฯและปริมณฑล ใช้เวลา 3 - 5 วันทำการ นับจากวันที่ ยืนยันการชำระเงิน</div>
+                      <div class="faq-font2">ต่างจังหวัด ใช้เวลา 5 - 7 วันทำการ นับจากวันที่ ยืนยันการชำระเงิน</div>
+                      <div class="faq-font2 pt-3">
+                        อย่างไรก็ตาม หากมีเหตุจำเป็นใดๆเกิดขึ้น ระยะเวลากาจัดส่งสินค้าอาจมีการเปลี่ยนปลงได้ ขอให้ท่านมั่นใจได้ว่า ทาง Lion Shop Online
+                        จะบริการลูกค้าทุกท่านอย่างดีที่สุด หากมีข้อสงสัยเพิ่มเติม ท่านสามารถโทรมาสอบถามได้จากศูนย์บริการลูกค้าของเราได้ตลอดเวลาทำการ...
+                      </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-12">
+                <button class="btn faq-btn" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
+                    <div class="faq-font1">
+                      <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                      เป็นลูกค้า Thai N.K. Plastic จะได้รับสิทธิพิเศษอะไรบ้าง
+                    </div>
+                </button>
+                <div class="collapse" id="collapseExample2">
+                    <div class="card card-body faq-bg">
+                      <div class="faq-font2">ช่วงเวลาการจัดส่งสินค้า คือ จันทร์ - ศุกร์ เวลา 8.00 - 16.00 น. โดยมี 2 ประเภทตามเขตการส่ง ดังนี้ค่ะ</div>
+                      <div class="faq-font2 pt-3">เขตกรุงเทพฯและปริมณฑล ใช้เวลา 3 - 5 วันทำการ นับจากวันที่ ยืนยันการชำระเงิน</div>
+                      <div class="faq-font2">ต่างจังหวัด ใช้เวลา 5 - 7 วันทำการ นับจากวันที่ ยืนยันการชำระเงิน</div>
+                      <div class="faq-font2 pt-3">
+                        อย่างไรก็ตาม หากมีเหตุจำเป็นใดๆเกิดขึ้น ระยะเวลากาจัดส่งสินค้าอาจมีการเปลี่ยนปลงได้ ขอให้ท่านมั่นใจได้ว่า ทาง Lion Shop Online
+                        จะบริการลูกค้าทุกท่านอย่างดีที่สุด หากมีข้อสงสัยเพิ่มเติม ท่านสามารถโทรมาสอบถามได้จากศูนย์บริการลูกค้าของเราได้ตลอดเวลาทำการ...
+                      </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-12">
+                <button class="btn faq-btn" type="button" data-toggle="collapse" data-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample3">
+                    <div class="faq-font1">
+                      <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                      เป็นลูกค้า Thai N.K. Plastic จะได้รับสิทธิพิเศษอะไรบ้าง
+                    </div>
+                </button>
+                <div class="collapse" id="collapseExample3">
+                    <div class="card card-body faq-bg">
+                      <div class="faq-font2">ช่วงเวลาการจัดส่งสินค้า คือ จันทร์ - ศุกร์ เวลา 8.00 - 16.00 น. โดยมี 2 ประเภทตามเขตการส่ง ดังนี้ค่ะ</div>
+                      <div class="faq-font2 pt-3">เขตกรุงเทพฯและปริมณฑล ใช้เวลา 3 - 5 วันทำการ นับจากวันที่ ยืนยันการชำระเงิน</div>
+                      <div class="faq-font2">ต่างจังหวัด ใช้เวลา 5 - 7 วันทำการ นับจากวันที่ ยืนยันการชำระเงิน</div>
+                      <div class="faq-font2 pt-3">
+                        อย่างไรก็ตาม หากมีเหตุจำเป็นใดๆเกิดขึ้น ระยะเวลากาจัดส่งสินค้าอาจมีการเปลี่ยนปลงได้ ขอให้ท่านมั่นใจได้ว่า ทาง Lion Shop Online
+                        จะบริการลูกค้าทุกท่านอย่างดีที่สุด หากมีข้อสงสัยเพิ่มเติม ท่านสามารถโทรมาสอบถามได้จากศูนย์บริการลูกค้าของเราได้ตลอดเวลาทำการ...
+                      </div>
+                    </div>
+                </div>
+             </div>
+          </div>
+
+
+
+
+
+
+
+
+            <!-- END CONTANER ------>
+        </div>
+        <!-- END CONTENT ---------->
+    </div>
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
