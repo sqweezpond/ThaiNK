19ea6a3888ba52a7320bf11f61b3e5e1548f4d78
gggia <gggia36@gmail.com>
2018-07-17 14:18:14 +0700

    uu

--- book.php
+++ book.php
@@ -13,47 +13,157 @@
 
     <div class="wrapper">
        <!-- CONTENT -->
-        <div class="container pb-5">
-          <div class="row">
+        
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8">
+      <table class=" table table-striped book-shado my-4 mt-5" >
+        <thead style="    background-color: #f3f3f2;">
+          <tr class="my-4">
+            <th><i class="ml-lg-4 fa fa-check-square text-center" aria-hidden="true"></i></th>
+            <th>สินค้า</th>
+            <th>ราคา</th>
+            <th>จำนวน</th>
+          </tr>
+        </thead>
+      </table>
+
+      <table class=" table  book-shado my-4" >
+        <thead>
+         <tbody>
+            <tr>
+              <td><i class="ml-lg-4 fa fa-check-square text-center" aria-hidden="true"></td>
+              <td class="book_frame-crad"><img class="book_card-img-top" src="https://3.imimg.com/data3/OE/QO/MY-6012314/pet-water-bottles-250x250.png"></td>
+              <td class="text-center">
+                <p>200 บาท</p>
+                <p>300</p>
+              </td>
+               <td>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <button class="book-btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                  </div>
+                  <input type="text" class="form-control text-center" placeholder="1" aria-label="Example text with button addon" aria-describedby="button-addon1">
+                  <div class="input-group-prepend">
+                    <button class="book-btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </thead>
+      </table>
+
+       <table class=" table  book-shado my-4" >
+        <thead>
+         <tbody>
+            <tr>
+              <td><i class="ml-lg-4 fa fa-check-square text-center" aria-hidden="true"></td>
+              <td class="book_frame-crad"><img class="book_card-img-top" src="https://png.pngtree.com/element_origin_min_pic/17/02/20/a34de64605fd6f52839a7bcf62b91571.jpg"></td>
+              <td class="text-center">
+                <p>200 บาท</p>
+                <p>300</p>
+              </td>
+               <td>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <button class="book-btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                  </div>
+                  <input type="text" class="form-control text-center" placeholder="1" aria-label="Example text with button addon" aria-describedby="button-addon1">
+                  <div class="input-group-prepend">
+                    <button class="book-btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </thead>
+      </table>
+
+
+    </div>
+
+    <div class="col-lg-4 my-4 mt-5">
+      <div class="row book-shado" >
+        <div class="col-lg-9  col-md-6 col-6">
+          <h5>ที่อยู่สำหรับจัดส่ง</h5>
+        </div>
+        <div class="col-lg-3 col-md-6 col-6 text-right">
+          <h2><i class="fa fa-pencil-square-o" aria-hidden="true"></i></h2>
+        </div>
+        <div class="col-lg-12">
+          <p class="ml-1 mt-3">180/2 ม.3 ซ.โยกย้าย ซอย 6 ถนน.บางกรวย-ไทรน้อย ต.บางใหญ่ อ.เมือง จ.นนทบุรี 11000</p>
+        </div>
+      </div>  
+
+      <div class="row book-shado my-4" >
+        <div class="col-lg-12">
+          <h5>สรุปรายการสั่งซื้อ</h5>
+        </div>
+        <div class="col-lg-12">
+          <p class="ml-1 mt-3">180/2 ม.3 ซ.โยกย้าย ซอย 6 ถนน.บางกรวย-ไทรน้อย ต.บางใหญ่ อ.เมือง จ.นนทบุรี 11000</p>
+        </div>
+        <div class="col-lg-9 col-md-6 col-6" style="line-height: 10px;">
+          <p>จำนวน</p>
+          <p>ยอดรวม</p>
+          <p>ค่าจัดส่ง</p>
+          <p>ส่วนลด</p>
+
+        </div>
+        <div class="col-lg-3 col-md-6 col-6 text-right" style="line-height: 10px;">
+          <p>4</p>
+          <p>900.00</p>
+          <p>100.00</p>
+          <p>-45.00</p>
+        </div>
+
+
+          <div class="col-lg-8 ">
+             <div class="form-group">
+                <input type="email" class="book-login-from" id="exampleFormControlInput1" placeholder="กรุณาระบุโค้ดส่วนลด">
+            </div>
+          </div>
+          <div class="col-lg-4 text-center">
+           <button type="submit" class="btn book-btn-daw">Submit</button>
+          </div>
+          <div class="col-lg-12">
+             <hr>
+          </div>
+         
+
+          <div class="col-lg-8  col-md-6 col-6">
+            <p>ยอดรวมทั้งหมด</p>
+          </div>
+          <div class="col-lg-4  col-md-6 col-6 text-right">
+            <h5 class="book-color">360 บาท</h5>
+          </div>
+          <div class="col-lg-12 text-right">
+            <p class="font-12">รวมภาษีมูลค่าเพิ่ม</p>
+          </div>
+           <div class="col-lg-12 text-center">
+            <button type="button" class="btn book-btn-100 ">ชำระเงิน</button>
+          </div>
+        </div>
+      </div>     
+    </div>
+    
+
 
           
 
-            <div class="col-lg-2">
-              <div>
-                <i class="fa fa-check-square text-center" aria-hidden="true"></i>
-              </div>
-            </div>
 
-
-
-            <div class="col-lg-5">
-              <div>
-                <p>สินค้า</p>
-              </div>
-              
-
-              
-            </div>
-            <div class="col-lg-3">
-              <div>
-                <p>ราคา</p>
-              </div>  
-            </div>
-
+  </div>
+</div>
+         
             
 
-            <div class="col-lg-2">
-              <div>
-                <p>จำนวน</p>
-              </div>   
-            </div>
-
-            
-           
 
 
 
-          </div>
+
+
+
 
 
 
