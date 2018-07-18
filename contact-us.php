220f3f3198f157fef3a1243d7cba4592b530df00
gggia <gggia36@gmail.com>
2018-07-18 16:34:31 +0700

    up

--- contact-us.php
+++ contact-us.php
@@ -121,7 +121,8 @@
 
   </body>
 </html>
-
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script type="text/javascript" src="https://rawgit.com/jackmoore/zoom/master/jquery.zoom.min.js"></script>
 <script>
   $(document).ready(function() {
     $('#zoom').zoom({
